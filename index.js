fec6c4c301cb8522c37b0edfb78abdc4b10c0168
Panuwat Yuenyong <59143204@g.cmru.ac.th>
2019-07-13 13:55:50 +0700

    Kyu2

---
+++ index.js
@@ -0,0 +1,10 @@
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => res.send('Hello MeawMii !'))
+app.get('/about', (req, res) => res.send('หน้า about !'))
+app.get('/aboutme', (req, res) => res.send('หน้า เกี่ยวกับเรา !'))
+app.get('/telme', (req, res) => res.send('หน้า ติดต่อเรา !'))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
