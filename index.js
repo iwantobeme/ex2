eacc5e38a5f81a15f47ef667341c80280a0900fc
Panuwat Yuenyong <59143204@g.cmru.ac.th>
2019-07-13 14:13:34 +0700

    เพิ่มหน้าเทส

--- index.js
+++ index.js
@@ -7,4 +7,9 @@ app.get('/', (req, res) => res.send('Hello MeawMii !'))
 app.get('/about', (req, res) => res.send('หน้า about !'))
 app.get('/aboutme', (req, res) => res.send('หน้า เกี่ยวกับเรา !'))
 app.get('/telme', (req, res) => res.send('หน้า ติดต่อเรา !'))
+app.get('/test', (req, res) => {
+    res.json({message:"test"})
+})
+
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
